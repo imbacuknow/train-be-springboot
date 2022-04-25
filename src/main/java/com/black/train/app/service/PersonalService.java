3cc69c7b7682ab10e3c2a7787570d6a0f6e85ff9
imbacuknow <imbac.smil3@gmail.com>
2022-04-25 17:29:13 +0700

    manage key and value to redis

--- src/main/java/com/black/train/app/service/PersonalService.java
+++ src/main/java/com/black/train/app/service/PersonalService.java
@@ -4,43 +4,47 @@ import com.black.train.app.entity.Personal;
 import com.black.train.app.exception.DataNotFoundException;
 import com.black.train.app.model.CreatePersonalRequest;
 import com.black.train.app.repository.PersonalRepository;
+import com.google.gson.Gson;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.cache.annotation.CacheEvict;
-import org.springframework.cache.annotation.CachePut;
-import org.springframework.cache.annotation.Cacheable;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
 
 @Service
-@Slf4j
 @RequiredArgsConstructor
 public class PersonalService {
 
     private PersonalRepository personalRepository;
+    private Gson gson;
+    private RedisTemplate redisTemplate;
+
 
     @Autowired
-    public PersonalService(PersonalRepository personalRepository) {
+    public PersonalService(PersonalRepository personalRepository, Gson gson, RedisTemplate redisTemplate) {
         this.personalRepository = personalRepository;
+        this.gson = gson;
+        this.redisTemplate = redisTemplate;
     }
 
     // ---------------- create ----------------
     public Personal createPersonal(CreatePersonalRequest personalRequest) {
-        log.info("Creating personal: {}", personalRequest);
         Personal personal = Personal.builder()
                 .name(personalRequest.getName())
                 .height(personalRequest.getHeight())
                 .weight(personalRequest.getWeight())
                 .age(personalRequest.getAge())
                 .build();
+        redisTemplate.keys("personal*").forEach(key -> redisTemplate.delete(key));
         return personalRepository.save(personal);
     }
     public List<Personal> createMultiplePersonal(List<CreatePersonalRequest> personalRequest) {
-        log.info("Creating multiple personal: {}", personalRequest);
         List<Personal> personals = new ArrayList<>();
         personalRequest.forEach(personal -> {
             Personal _personal = Personal.builder()
@@ -51,49 +55,67 @@ public class PersonalService {
                     .build();
             personals.add(_personal);
         });
+        redisTemplate.keys("personal*").forEach(key -> redisTemplate.delete(key));
         return personalRepository.saveAll(personals);
     }
 
     // ---------------- read ----------------
     public List<Personal> retrieveAllPersonal() {
-        log.info("Retrieving all personal");
-        return personalRepository.findAll();
+        Object personals = redisTemplate.opsForValue().get("personals");
+        if (personals != null) {
+            System.out.println("data is exist in redis");
+            List<Personal> objList = gson.fromJson(personals.toString(), List.class);
+            return objList;
+        } else {
+            System.out.println("data not exist in redis");
+            List<Personal> objList = personalRepository.findAll();
+            redisTemplate.opsForValue().set("personals", gson.toJson(objList), 60, TimeUnit.SECONDS);
+            return objList;
+        }
     }
 
     public Personal retrievePersonalById(Long id) {
-        log.info("Retrieving personal with id: {}", id);
-        return personalRepository.findById(id).orElse(null);
+        return personalRepository.findById(id).orElseThrow(() -> new DataNotFoundException(("Personal id " + id + " not found")));
     }
 
     public List<Personal> retrievePersonalAgeUnder50() {
-        log.info("Retrieving personal age under 50");
-        List<Personal> personalAgeUnder50 = personalRepository.findAll()
-                .stream()
-                .filter(p -> p.getAge() < 50)
-                .collect(Collectors.toList());
-        return personalAgeUnder50;
+        Object personals = redisTemplate.opsForValue().get("personalAgeUnder50");
+
+        if (personals != null) {
+            System.out.println("data is exist in redis");
+            List<Personal> objList = gson.fromJson(personals.toString(), List.class);
+            return objList;
+        } else {
+            System.out.println("data not exist in redis");
+            List<Personal> objList = personalRepository.findAll()
+                    .stream()
+                    .filter(p -> p.getAge() < 50)
+                    .collect(Collectors.toList());
+            redisTemplate.opsForValue().set("personalAgeUnder50", gson.toJson(objList), 60, TimeUnit.SECONDS);
+            return objList;
+        }
     }
 
     // ---------------- update ----------------
     public Personal updatePersonal(CreatePersonalRequest personalRequest) {
-        log.info("Updating personal with id: {}", personalRequest);
-        Personal personal = personalRepository.findById(personalRequest.getId()).orElseThrow(() -> new DataNotFoundException("Personal not found"));
+        Personal personal = personalRepository.findById(personalRequest.getId()).orElseThrow(() -> new DataNotFoundException("Personal id " + personalRequest.getId() + " not found"));
         personal.setName(personalRequest.getName());
         personal.setHeight(personalRequest.getHeight());
         personal.setWeight(personalRequest.getWeight());
         personal.setAge(personalRequest.getAge());
+        redisTemplate.keys("personal*").forEach(key -> redisTemplate.delete(key));
         return personalRepository.save(personal);
     }
 
     // ---------------- delete ----------------
     public void deletePersonal(Long id) {
-        log.info("Deleting personal with id: {}", id);
-        Personal personal = personalRepository.findById(id).orElseThrow(() -> new DataNotFoundException("Personal not found"));
+        Personal personal = personalRepository.findById(id).orElseThrow(() -> new DataNotFoundException("Personal id " + id + " not found"));
         personalRepository.deleteById(id);
+        redisTemplate.keys("personal*").forEach(key -> redisTemplate.delete(key));
     }
 
     public void deleteAllPersonal() {
-        log.info("Deleting all personal");
         personalRepository.deleteAll();
+        redisTemplate.keys("personal*").forEach(key -> redisTemplate.delete(key));
     }
 }
